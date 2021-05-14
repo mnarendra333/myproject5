d2210797291d7349663419fad0cf0bc787779a27
Narendra <nmunnang@cisco.com>
2021-05-14 23:26:32 +0530

    first commit

---
+++ src/main/java/com/infy/myproject5/Myproject5Application.java
@@ -0,0 +1,13 @@
+package com.infy.myproject5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myproject5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myproject5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/infy/myproject5/Myproject5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.myproject5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myproject5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
